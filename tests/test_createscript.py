e7b2f26964d1414dbe8d52a7b20827196f272d4f
John Tran <johntran@eightdawns.com>
2023-11-03 22:04:26 -0400

    added SG support

--- tests/test_createscript.py
+++ tests/test_createscript.py
@@ -9,7 +9,10 @@ from shotgrid_orm import SGORM, SchemaType
 
 ECHO = False
 
-sg_orm = SGORM(sg_schema_type=SchemaType.JSON, sg_schema_source="schema.json", echo=ECHO)
-print("creating python script")
-sg_orm.create_script("sgmodel.py")
+# sg_orm = SGORM(sg_schema_type=SchemaType.JSON_FILE, sg_schema_source="schema.json", echo=ECHO)
+# print("creating python script")
+# sg_orm.create_script("sgmodel.py")
 
+sg_orm = SGORM(sg_schema_type=SchemaType.SG_SCRIPT, sg_schema_source="schema.json", echo=ECHO)
+print("creating python script")
+sg_orm.create_script("sgmodel1.py")
