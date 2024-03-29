5cd97791a03795940da68550ae2c8961bc9d57bd
John Tran <johnemtran@gmail.com>
2024-03-29 15:44:07 -0400

    added comments

--- tests/test_createscript.py
+++ tests/test_createscript.py
@@ -9,11 +9,13 @@ from shotgrid_orm import SGORM, SchemaType
 
 ECHO = False
 
+# create python script from JSON schema file
 # sg_orm = SGORM(sg_schema_type=SchemaType.JSON_FILE, sg_schema_source="schema.json", echo=ECHO)
 # print("creating python script")
 # sg_orm.create_script("sgmodel.py")
 
+# create python script from live connection using SCRIPT KEY
 sg_orm = SGORM(sg_schema_type=SchemaType.SG_SCRIPT, sg_schema_source={"url": os.getenv("SG_URL"), "script": os.getenv("SG_SCRIPT"), "api_key": os.getenv("SG_API_KEY")}, echo=ECHO)
 print("creating python script")
-sg_orm.create_script("sgmodel1.py")
+sg_orm.create_script("sgmodel.py")
 
